3cee9833c4e2d6f484fef99d5a26bb49f1b9483c
Azucena <15470138@itcampeche.edu.mx>
2018-12-11 18:50:27 -0600

    IMC: version inicial. interfaz de usuario/no contiene manejo de eventos.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 #App que calcula el Indice de Masa Corporal.
+#app que calcula el Indice de Masa Corporal.
+#Aplicacion que calcula el INDICE DE MASA CORPORAL.
--- app/src/main/java/com/azucenalopez/android/imc/MainActivity.java
+++ app/src/main/java/com/azucenalopez/android/imc/MainActivity.java
@@ -8,42 +8,5 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    private EditText campoPeso;
-    private EditText campoEstatura;
-    private Button botonCalcular;
-    private Button botonLimpiar;
-    private TextView textoImc2;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        campoPeso = (EditText) findViewById(R.id.campo_peso);
-        campoEstatura = (EditText) findViewById(R.id.campo_estatura);
-        botonCalcular = (Button) findViewById(R.id.boton_calcular);
-        botonLimpiar = (Button) findViewById(R.id.boton_limpiar);
-        textoImc2 = (TextView) findViewById(R.id.etiqueta_imc);
-
-        botonCalcular.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String s = campoPeso.getText().toString();
-                double peso = Double.parseDouble(s);
-                s = campoEstatura.getText().toString();
-                double estatura = Double.parseDouble(s);
-                double imc = peso / (estatura * estatura);
-                s = String.format("%2.2f", imc);
-                textoImc2.setText(s);
-            }
-        });
-        botonLimpiar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                campoPeso.setText("");
-                campoEstatura.setText("");
-                textoImc2.setText("0.0");
-            }
-        });
-    }
 }
